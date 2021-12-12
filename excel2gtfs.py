cc88a93d33b563be90e8a2e4eb770fce9ebd5c29
Jeff Kessler <email@jeffkess.com>
2021-12-12 09:54:30 -0500

    [0.0.2] Schedule data processing

--- excel2gtfs.py
+++ excel2gtfs.py
@@ -1,8 +1,9 @@
 """"------------------------------------------------------------------
-Excel2GTFS v0.0.1
-(c) Jeff Kessler, 2021-12-12-0745
+Excel2GTFS v0.0.2
+(c) Jeff Kessler, 2021-12-12-0950
 
 0.0.1  Initial Commit
+0.0.2  Schedule data processing
 ------------------------------------------------------------------"""
 
 import openpyxl
@@ -10,10 +11,10 @@ import csv
 import os
 import time
 
-wb = openpyxl.load_workbook(filename="ExcelGTFS.xlsx")
+wb = openpyxl.load_workbook(filename="ExcelGTFS.xlsx", data_only=True)
 
 # Identify applicable sheets
-config_sheets = {"Agency", "Routes", "Stops", "Fare Rules", "Fares", "Calendar", "Calendar Dates"}
+config_sheets = {"Agency", "Routes", "Stops", "Fare Rules", "Fares", "Calendar", "Calendar Dates", "Shapes"}
 services = set(wb.sheetnames) - config_sheets
 config_sheets = config_sheets.intersection(wb.sheetnames)
 
@@ -23,7 +24,7 @@ os.makedirs(fp)
 
 
 # ------------------------------------------------------------------------------
-# Process Configuration Sheets
+# Process and Write Configuration Sheets
 # ------------------------------------------------------------------------------
 
 for sheet_name in config_sheets:
@@ -33,3 +34,65 @@ for sheet_name in config_sheets:
         writer.writerows([row for row in wb[sheet_name].values])
 
 
+# ------------------------------------------------------------------------------
+# Process Schedule Data
+# ------------------------------------------------------------------------------
+
+# Initialize Services
+special_keys = ["route_id", "direction_id", "shape_id", "headsign", "wheelchair_accessible", "bikes_allowed", "Then Every", "Until"]
+gtfs_entries = {"trips": [], "stop_times": [], "frequencies": []}
+
+# Process Schedule Sheets
+for service in services:
+
+    svc_trips = list(wb[service].values)
+    svc_trip_dicts = [{svc_trips[1][index]: val for index, val in enumerate(row)} for row in svc_trips[2:] if any(row)]
+
+    for trip in svc_trip_dicts:
+
+        # Identify Stops and Define trip_id by Origin and Departure Time
+        trip_stop_times = sorted([(key, val.strftime("%H:%M:%S")) for key, val in trip.items() if key not in special_keys and val], key=lambda x: x[-1])
+        trip_id = "-".join(str(item) for item in [service, *trip_stop_times[0]])
+
+        # Append trips.txt Entries
+        gtfs_entries["trips"].append({
+            "trip_id": trip_id,
+            "route_id": trip.get("route_id"),
+            "direction_id": trip.get("direction_id"),
+            "shape_id": trip.get("shape_id", ""),
+            "trip_headsign": trip.get("headsign", ""),
+            "wheelchair_accessible": trip.get("wheelchair_accessible", ""),
+            "bikes_allowed": trip.get("bikes_allowed", ""),
+        })
+
+        # Append stop_time.txt Entries
+        [gtfs_entries["stop_times"].append({
+            "trip_id": trip_id,
+            "arrival_time": val[1],
+            "departure_time": val[1],
+            "stop_id": str(val[0]),
+            "stop_sequence": index,
+            "pickup_type": "1" if index==len(trip_stop_times)-1 else "0",
+            "drop_off_type": "1" if index==0 else "0"
+        }) for index, val in enumerate(trip_stop_times)]
+
+        # Append frequencies.txt Entries
+        if trip.get("Then Every") and trip.get("Until"):
+            gtfs_entries["frequencies"].append({
+                "trip_id": trip_id,
+                "start_time": trip_stop_times[0][-1],
+                "end_time": trip["Until"].strftime("%H:%M:%S"),
+                "headway_secs": (trip["Then Every"].hour*60*60 + trip["Then Every"].minute*60 + trip["Then Every"].second),
+                "exact_times": "1"
+            })
+
+# ------------------------------------------------------------------------------
+# Write Schedule Data
+# ------------------------------------------------------------------------------
+
+for key in gtfs_entries:
+    if gtfs_entries[key]:
+        with open(f'{fp}/{key}.csv', "w") as file:
+            writer = csv.DictWriter(file, fieldnames=list(gtfs_entries[key][0]))
+            writer.writeheader()
+            writer.writerows(gtfs_entries[key])
